5abfa6a654d19374915eb6122c5f57acba7d6223
vtulpin <63840961+vtulpin@users.noreply.github.com>
2020-11-08 15:37:59 +0300

    1Homework
    1-4 numbers

---
+++ MATHhw3.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from math import factorial
+
+
+# In[2]:
+
+
+a = 2/3
+
+
+# In[3]:
+
+
+b = (2/3)*(9/14)
+
+
+# In[5]:
+
+
+b
+
+
+# In[11]:
+
+
+P = b*(8/13)
+
+
+# In[12]:
+
+
+P
+
+
+# In[ ]:
+
+
+
+
