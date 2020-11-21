43fc82d5323893f0a4f101b3a2aeae88830f8f97
vtulpin <63840961+vtulpin@users.noreply.github.com>
2020-11-21 21:11:34 +0300

    Add files via upload

---
+++ Untitled15.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import numpy.random as rand
+
+
+# In[3]:
+
+
+a = np.random.uniform(200, 800, (2, 10))
+
+
+# In[4]:
+
+
+a
+
+
+# In[7]:
+
+
+np.mean(a) #ср значение 
+
+
+# In[8]:
+
+
+np.var(a) #дисперсия
+
+
+# In[ ]:
+
+
+
+
---
+++ Untitled16.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+D = 0.2
+a = 0.5
+
+
+# In[11]:
+
+
+b = 2
+
+
+# In[12]:
+
+
+import numpy as np
+import numpy.random as rand
+
+
+# In[13]:
+
+
+x = np.random.uniform(0.5, 2, (1, 5))
+
+
+# In[14]:
+
+
+x
+
+
+# In[15]:
+
+
+np.mean(x)
+
+
+# In[ ]:
+
+
+
+
